557ef2c01819964de727a2443aca24b4d1ace901
Fábio Ceriaco <ceriacofabio@gmail.com>
2026-01-13 10:38:29 +0000

    Cosrmiddleware

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.db.session import init_db
 from api.events import router as events_router
 from api.db.session import init_db
@@ -15,6 +16,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(events_router, prefix="/api/events")
 
 
